3fe12c13405038837d7dbbdc863dd53fb81c6022
Tristanwyatt <tristan7306@gmail.com>
2020-09-04 16:02:45 -0500

    api done

--- scripts/JournalDataProvider.js
+++ scripts/JournalDataProvider.js
@@ -1,44 +1,40 @@
-/*
- *   Journal data provider for Daily Journal application
- *
- *      Holds the raw data about each entry and exports
- *      functions that other modules can use to filter
- *      the entries for different purposes.
- */
+const eventHub = document.querySelector('.mainbody');
 
-// This is the original data.
-const journal = [
-    {
-        id: 1,
-        date: "07/24/2025",
-        concept: "HTML & CSS",
-        entry: "We talked about HTML components and how to make grid layouts with Flexbox in CSS.",
-        mood: "Ok"
-    },
-    {
-        id: 2,
-        date: "06/24/2025",
-        concept: "HTML & CSS",
-        entry: "We talked about HTML components and how to make grid layouts with Flexbox in CSS.",
-        mood: "Ok"
-    },
-    {
-        id: 3,
-        date: "09/24/2025",
-        concept: "HTML & CSS",
-        entry: "We talked about HTML components and how to make grid layouts with Flexbox in CSS.",
-        mood: "Ok"
-    }
-]
 
-/*
-    You export a function that provides a version of the
-    raw data in the format that you want
-*/
-export const useJournalEntries = () => {
-    const sortedByDate = journal.sort(
-        (currentEntry, nextEntry) =>
-            Date.parse(currentEntry.date) - Date.parse(nextEntry.date)
-    )
-    return sortedByDate
+
+
+const dispatchStateChangeEvent = () => {
+    const newJournalRecorded = (new CustomEvent("journalStateChanged"))
+
+    eventHub.dispatchEvent(newJournalRecorded)
+}
+
+let entry;
+
+export const useEntries = () => {
+    return entry.slice();
+}
+
+
+export const saveJournalEntry = (newJournalEntry) => {
+fetch("http://localhost:8088/entries", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify(newJournalEntry)
+})
+    .then(getEntries) 
+    .then(dispatchStateChangeEvent)
+
+}
+
+
+export const getEntries = () => {
+    return fetch("http://localhost:8088/entries") 
+        .then(response => response.json())  
+        .then(entries => {
+            entry = entries;
+            
+        })
 }
--- scripts/JournalEntry.js
+++ scripts/JournalEntry.js
@@ -1,7 +1,4 @@
-/*
- *  Purpose: To render a single journal entry as an
- *           HTML representation of the data
- */
+
 export const JournalEntryComponent = (entry) => {
     return `
         <section id="entry${entry.id}" class="journalEntry">
